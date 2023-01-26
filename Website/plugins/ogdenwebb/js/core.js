8f5b8932b5d69e73a96585ac4e4667397f1fad75
finanalyst <rnhainsworth@gmail.com>
2023-01-26 21:35:01 +0000

    use localStorage for Sidebar instead of cookie

--- Website/plugins/ogdenwebb/js/core.js
+++ Website/plugins/ogdenwebb/js/core.js
@@ -43,38 +43,23 @@ $(document).ready( function() {
     });
 
 $(document).ready( function() {
-    sidebar_is_shown = JSON.parse(cookie.get('sidebar', null));
+    var sidebar_is_shown = localStorage.getItem('sidebarIsShown');;
     if (sidebar_is_shown === null) {
         // If the screen is not wide enough and the sidebar overlaps content -
         // hide it (if it was not enabled on purpose and so was in cookies)
-        sidebar_is_shown = $(window).width() <= 1760;
-        // opposite so the toggle reverses it
-        cookie.set({ sidebar: sidebar_is_shown }, { expires: 30, path: '/', sameSite: 'lax', secure: true });
+        sidebar_is_shown = $(window).width() > 1760 ? 'open' : 'closed';
+        localStorage.setItem('sidebarIsShown', sidebar_is_shown);
     }
     move_sidebar( sidebar_is_shown );
     $('#raku-sidebar').click( function() {
-        sidebar_is_shown = ! sidebar_is_shown;
-        cookie.set({ sidebar: sidebar_is_shown },
-            { expires: 30, path: '/', sameSite: 'lax', secure: true });
+        sidebar_is_shown = sidebar_is_shown === 'closed' ? 'open' : 'closed';
+        localStorage.setItem('sidebarIsShown', sidebar_is_shown);
         move_sidebar( sidebar_is_shown );
     });
     function move_sidebar( show ) {
         var el = $('#raku-sidebar');
         var svg = $(el).find('svg')[0];
-        if (sidebar_is_shown) {
-            $("#mainSidebar").css('width', '0');
-            $("#mainSidebar").css('display', 'none');
-            $(el).css('left', '-5px');
-//            if (svg !== undefined) {
-                svg.setAttribute('data-icon', 'chevron-right');
-//            }
-//            else {
-//                var i_icon = $(el).find('i')[0];
-//                $(i).removeClass('fa-chevron-left');
-//                $(i).addClass('fa-chevron-right');
-//            }
-        }
-        else {
+        if ( show === 'open' ) {
             $("#mainSidebar").css('width', '');
             $("#mainSidebar").css('display', 'block');
             $(el).css('left', '');
@@ -87,60 +72,48 @@ $(document).ready( function() {
 //                $(i).addClass('fa-chevron-left');
 //            }
         }
+        else {
+            $("#mainSidebar").css('width', '0');
+            $("#mainSidebar").css('display', 'none');
+            $(el).css('left', '-5px');
+//            if (svg !== undefined) {
+                svg.setAttribute('data-icon', 'chevron-right');
+//            }
+//            else {
+//                var i_icon = $(el).find('i')[0];
+//                $(i).removeClass('fa-chevron-left');
+//                $(i).addClass('fa-chevron-right');
+//            }
+        }
     };
-    $(".menu-list li").each(function (i, elLi) {
-    $(elLi).find('a').each(function (i, elA) {
-        $(elA).click(function () {
-            // Update menu items
-            $(".menu-list li").each(function (i, el) {
-                $(el).find('a').each(function (i, el) { $(el).removeClass('is-active'); });
-            });
-            $(this).addClass('is-active');
-            // Update tab visibility
-            var category = $(elLi).attr('id').substring(7); // 7 is length of "switch-"
-            var tab_id = 'tab-' + category;
-            $('.tabcontent').each(function (i, el) { $(el).css('display', 'none'); });
-            $('#' + tab_id).css('display', 'block');
-            // Update title-subtitle
-            $('.page-title').text($('#page-title-' + category).text());
-            $('.page-subtitle').text($('#page-subtitle-' + category).text());
-            // Update URL as well to follow convention of static pages like `type-basic.html` we rendered
-            // since forever, so backward-compat-y thing
-            let prefix = window.location.href;
-            let prefixEnd = prefix.includes('type') ? prefix.lastIndexOf('type') + 4 : prefix.lastIndexOf('routine') + 7;
-            history.pushState(null, null, prefix.substr(0, prefixEnd) + (category === 'all' ? '' : '-' + category));
-        });
-    });
-});
 
     var originalTOC = $('#toc-menu').html();
-
     $("#toc-filter").keyup(function () {
-    $('#toc-menu').html(originalTOC);
-    var searchText = this.value.toLowerCase();
-    if (searchText.length === 0) return;
-    var $menuListElements = $('.menu-list').find("li");
-    var $matchingListElements = $menuListElements.filter(function (i, li) {
-        var listItemHTML = li.firstChild.innerHTML;
-        var fuzzyRes = fuzzysort.go(searchText, [listItemHTML])[0];
-        if (fuzzyRes === undefined || fuzzyRes.score < -8000) {
-            return false;
-        }
-        var res = fuzzysort.highlight(fuzzyRes);
-        if (res !== null) {
-            var nodes = $(li).contents().filter(function (i, node) { return node.nodeType == 1; });
-            nodes[0].innerHTML = res;
-            return true;
-        } else {
-            return false;
-        }
+        $('#toc-menu').html(originalTOC);
+        var searchText = this.value.toLowerCase();
+        if (searchText.length === 0) return;
+        var $menuListElements = $('.menu-list').find("li");
+        var $matchingListElements = $menuListElements.filter(function (i, li) {
+            var listItemHTML = li.firstChild.innerHTML;
+            var fuzzyRes = fuzzysort.go(searchText, [listItemHTML])[0];
+            if (fuzzyRes === undefined || fuzzyRes.score < -8000) {
+                return false;
+            }
+            var res = fuzzysort.highlight(fuzzyRes);
+            if (res !== null) {
+                var nodes = $(li).contents().filter(function (i, node) { return node.nodeType == 1; });
+                nodes[0].innerHTML = res;
+                return true;
+            } else {
+                return false;
+            }
+        });
+        $menuListElements.hide();
+        $($matchingListElements).each(function (i, elem) {
+            $(elem).parents('li').show();
+        });
+        $matchingListElements.show();
     });
-    $menuListElements.hide();
-    $($matchingListElements).each(function (i, elem) {
-        $(elem).parents('li').show();
-    });
-    $matchingListElements.show();
-});
 });
 $(document).ready(function() {
     $('#query').focus(function () {
