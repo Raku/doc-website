239df5f1391d2e19a8b2092b3fce7e7fcd939e20
Rawley <75388349+rawleyfowler@users.noreply.github.com>
2023-02-07 09:39:14 +0000

    Modernize search javascript (#83)
    * Stop hovering effects on items you can't click
    
    * update js
    
    * remove merge conflict
    
    * Fix <<<<<<

--- Website/plugins/search-bar/search-temp.js
+++ Website/plugins/search-bar/search-temp.js
@@ -1,14 +1,13 @@
 //ITEMs to be added
-// var searchSite to be added
-var current_search = "";
-var category_search = (function() {
-    var method_sign = new RegExp(/^(\.)(\w[\w\-]+)/);
-    var routine_sign = new RegExp(/^(\&)(\w[\w-]+.*)/);
-    var routineMethod_sign = new RegExp(/([^\(]+)(\(\)?)$/);
-    var classPackageRole_sign = new RegExp(/^(\:\:)([A-Z][\w\:]*)/);
+let current_search = "";
+const category_search = (function() {
+    const method_sign = new RegExp(/^(\.)(\w[\w\-]+)/);
+    const routine_sign = new RegExp(/^(\&)(\w[\w-]+.*)/);
+    const routineMethod_sign = new RegExp(/([^\(]+)(\(\)?)$/);
+    const classPackageRole_sign = new RegExp(/^(\:\:)([A-Z][\w\:]*)/);
     return {
         filter_by_category: function(search_term, items) {
-            var filteredItems = [];
+            let filteredItems = [];
             if (search_term.match(method_sign)) {
                 filteredItems = items.filter(function(item) { return item.category.toLowerCase() === 'methods' ||  item.category.toLowerCase() === 'routines' });
             } else if (search_term.match(routine_sign)){
@@ -23,7 +22,7 @@ var category_search = (function() {
             return filteredItems;
         },
         strip_sign: function(search_term) {
-            var match;
+            let match;
             if (search_term.match(method_sign)) {
                 // We matched `.`, strip it off
                 search_term = search_term.substring(1);
@@ -48,18 +47,18 @@ $(function(){
       this.widget().menu( "option", "items", "> :not(.ui-autocomplete-category)" );
     },
     _renderItem: function( ul, item) {
-        var enter_text = $('<span>')
+        const enter_text = $('<span>')
             .attr('class', 'enter-prompt')
             .css('display', 'none')
             .html('Enter to select');
-        var info_element = $('<span>')
+        const info_element = $('<span>')
             .attr('class', 'item-info')
             .html( item.info );
-        var regex = new RegExp('('
+        const regex = new RegExp('('
             + current_search.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&')
             + ')', 'ig');
-        var text = item.label.replace(regex, '<b>$1</b>');
-        var boldMatch = text.match(/^<b>.*?<\/b>$/);
+        const text = item.label.replace(regex, '<b>$1</b>');
+        const boldMatch = text.match(/^<b>.*?<\/b>$/);
         if (boldMatch && boldMatch[0].match(/<b>/g).length === 1) {
             $('#navbar-search').attr('data-default-url', item.url);
             enter_text.css('display', 'inline');
@@ -84,14 +83,14 @@ $(function(){
             )
     },
     _renderMenu: function( ul, items ) {
-      var that = this,
-      currentCategory = "";
+      const that = this;
+      let currentCategory = "";
       $('#navbar-search').attr('data-default-url', '');
       $('#navbar-search').attr('data-search-url', '');
       function sortBy(a, b) {
         // We want to place 5to6 docs to the end of the list.
         // See if either a or b are in 5to6 category.
-        var isp5a = false, isp5b = false;
+        const isp5a = false, isp5b = false;
         if ( a.category.substr(0,4) == '5to6' ) { isp5a = true; }
         if ( b.category.substr(0,4) == '5to6' ) { isp5b = true; }
 
@@ -103,9 +102,9 @@ $(function(){
         // Sort by category alphabetically; 5to6 items would both have
         // the same category if we reached this point and category sort
         // will happen only on non-5to6 items
-        var a_cat = a.category.toLowerCase();
-        var b_cat = b.category.toLowerCase();
-        // put category Heading at the end
+        const a_cat = a.category.toLowerCase();
+        const b_cat = b.category.toLowerCase();
+       // put category Heading at the end
         if (a_cat == 'heading' && b_cat != 'heading') {return 1}
         if (a_cat != 'heading' && b_cat == 'heading') {return -1}
         // now sort normally
@@ -115,15 +114,15 @@ $(function(){
         // We reach this point when categories are the same; so
         // we sort items by value
 
-        var a_val = a.value.toLowerCase();
-        var b_val = b.value.toLowerCase();
+        const a_val = a.value.toLowerCase();
+        const b_val = b.value.toLowerCase();
 
         // exact matches preferred
         if ( a_val == current_search) {return -1}
         if ( b_val == current_search) {return  1}
 
-        var a_sw = a_val.startsWith(current_search);
-        var b_sw = b_val.startsWith(current_search);
+        const a_sw = a_val.startsWith(current_search);
+        const b_sw = b_val.startsWith(current_search);
         // initial matches preferred
         if (a_sw && !b_sw) { return -1}
         if (b_sw && !a_sw) { return  1}
@@ -134,16 +133,16 @@ $(function(){
 
         return 0;
       }
-      var sortedItems = items.sort(sortBy);
-      var keywords = category_search.strip_sign($("#query").val());
+      const sortedItems = items.sort(sortBy);
+      const keywords = category_search.strip_sign($("#query").val());
       sortedItems.push({
           category: 'Site Search',
           label: "Search the entire site for " + keywords,
           value: keywords,
           url: siteSearchUrl( keywords )
       });
-      $.each( sortedItems, function( index, item ) {
-        var li;
+      sortedItems.forEach(function(item, index) {
+        let li;
         if ( item.category != currentCategory ) {
           ul.append( "<li class='ui-autocomplete-category'>" + item.category + "</li>" );
           currentCategory = item.category;
@@ -181,7 +180,7 @@ $(function(){
         }
       },
       open: function() {
-        var ui_el = $('.ui-autocomplete');
+        const ui_el = $('.ui-autocomplete');
         if ( ui_el.offset().left < 0 ) {
             ui_el.css({left: 0})
         }
@@ -189,18 +188,18 @@ $(function(){
       },
       position: { my: "right top", at: "right bottom" },
       source: function(request, response) {
-          var filteredItems = category_search.filter_by_category(request.term, items);
-          var results = $.ui.autocomplete.filter(filteredItems, category_search.strip_sign(request.term));
+          const filteredItems = category_search.filter_by_category(request.term, items);
+          const results = $.ui.autocomplete.filter(filteredItems, category_search.strip_sign(request.term));
           function trim_results(results, term) {
-              var cutoff = 50;
+              const cutoff = 50;
               if (results.length < cutoff) {
                   return results;
               }
               // Prefer exact matches, then starting matches.
-              var exacts = [];
-              var prefixes = [];
-              var rest = [];
-              for (var ii = 0; ii <results.length; ii++) {
+              const exacts = [];
+              const prefixes = [];
+              const rest = [];
+              for (let ii = 0; ii <results.length; ii++) {
                   if (results[ii].value.toLowerCase() == term.toLowerCase()) {
                       exacts.push(ii);
                   } else if (results[ii].value.toLowerCase().startsWith(term.toLowerCase())) {
@@ -209,8 +208,8 @@ $(function(){
                       rest.push(ii);
                   }
               }
-              var keeps = [];
-              var pos = 0;
+              const keeps = [];
+              let pos = 0;
               while (keeps.length <= cutoff && pos < exacts.length) {
                   keeps.push(exacts[pos++]);
               }
@@ -222,7 +221,7 @@ $(function(){
               while (keeps.length <= cutoff && pos < rest.length) {
                   keeps.push(rest[pos++]);
               }
-              var filtered = [];
+              const filtered = [];
               for (pos = 0; pos < results.length; pos++) {
                   if (keeps.indexOf(pos) != -1) {
                       filtered.push(results[pos]);
@@ -249,7 +248,7 @@ var followLink = function() {
     /* When using return key to select, the select event
     and keydown event are both activated and the second
     event should do nothing */
-    var url;
+    let url;
     if ($('#navbar-search').attr('data-default-url')) {
         url = $('#navbar-search').attr('data-default-url');
         $('#navbar-search').attr('data-default-url', '');
@@ -279,16 +278,16 @@ $.extend( $.ui.autocomplete, {
             term = term.substr(1);
         }
 
-        var len = term.length;
-        var matcher = new RegExp( $.ui.autocomplete.escapeRegex( term ), "i" );
-        var OK_distance = len > 9 ? 4 : len > 6 ? 3 : len > 4 ? 2 : 1;
+        const len = term.length;
+        const matcher = new RegExp( $.ui.autocomplete.escapeRegex( term ), "i" );
+        const OK_distance = len > 9 ? 4 : len > 6 ? 3 : len > 4 ? 2 : 1;
         return $.grep( array, function( value ) {
             if (search_method && value.category != 'Method') {
                 return false;
             }
             if (len >=2 ) {
-                var result = sift4( value.value, term, 4, 0);
-                if (result <=OK_distance) {
+                const result = sift4( value.value, term, 4, 0);
+                if (result <= OK_distance) {
                     return true;
                 }
             }
@@ -319,7 +318,7 @@ function sift4(s1, s2, maxOffset, maxDistance) {
         return s2.length;
     }
 
-    if (!s2||!s2.length) {
+    if (!s2 || !s2.length) {
         return s1.length;
     }
 
@@ -336,19 +335,18 @@ function sift4(s1, s2, maxOffset, maxDistance) {
     while ((c1 < l1) && (c2 < l2)) {
         if (s1.charAt(c1) == s2.charAt(c2)) {
             local_cs++;
-            var isTrans=false;
+            let isTrans = false;
             //see if current match is a transposition
-            var i=0;
-            while (i<offset_arr.length) {
-                var ofs=offset_arr[i];
+            let i=0;
+            for(let i = 0; i < offset_arr.length; i++) {
+                const ofs=offset_arr[i];
                 if (c1<=ofs.c1 || c2 <= ofs.c2) {
                     // when two matches cross, the one considered a transposition is the one with the largest difference in offsets
-                    isTrans=Math.abs(c2-c1)>=Math.abs(ofs.c2-ofs.c1);
+                    isTrans = Math.abs(c2 - c1) >= Math.abs(ofs.c2 - ofs.c1);
                     if (isTrans)
                     {
                         trans++;
-                    } else
-                    {
+                    } else {
                         if (!ofs.trans) {
                             ofs.trans=true;
                             trans++;
@@ -356,7 +354,7 @@ function sift4(s1, s2, maxOffset, maxDistance) {
                     }
                     break;
                 } else {
-                    if (c1>ofs.c2 && c2>ofs.c1) {
+                    if (c1 > ofs.c2 && c2 > ofs.c1) {
                         offset_arr.splice(i,1);
                     } else {
                         i++;
@@ -369,16 +367,16 @@ function sift4(s1, s2, maxOffset, maxDistance) {
                 trans:isTrans
             });
         } else {
-            lcss+=local_cs;
-            local_cs=0;
+            lcss += local_cs;
+            local_cs = 0;
             if (c1!=c2) {
-                c1=c2=Math.min(c1,c2);  //using min allows the computation of transpositions
+                c1 = c2=Math.min(c1,c2);  //using min allows the computation of transpositions
             }
             //if matching characters are found, remove 1 from both cursors (they get incremented at the end of the loop)
             //so that we can have only one code block handling matches
-            for (var i = 0; i < maxOffset && (c1+i<l1 || c2+i<l2); i++) {
+            for (let i = 0; i < maxOffset && (c1+i<l1 || c2+i<l2); i++) {
                 if ((c1 + i < l1) && (s1.charAt(c1 + i) == s2.charAt(c2))) {
-                    c1+= i-1;
+                    c1 += i-1;
                     c2--;
                     break;
                 }
@@ -391,9 +389,8 @@ function sift4(s1, s2, maxOffset, maxDistance) {
         }
         c1++;
         c2++;
-        if (maxDistance)
-        {
-            var temporaryDistance=Math.max(c1,c2)-lcss+trans;
+        if (maxDistance) {
+            let temporaryDistance=Math.max(c1,c2)-lcss+trans;
             if (temporaryDistance>=maxDistance) return Math.round(temporaryDistance);
         }
         // this covers the case where the last match is on the last token in list, so that it can compute transpositions correctly
