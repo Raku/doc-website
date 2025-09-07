ffc1974a4994da6f63f466ddce4da8d0269540d1
Coleman McFarland <coleman@de.limited>
2025-09-07 18:41:58 -0400

    Add expiry in old script

--- .buildkite/container.sh
+++ .buildkite/container.sh
@@ -3,10 +3,21 @@
 set -eu
 
 image_tag=$1
+version_tag="v1-$(date +%Y%m%d)-${BUILDKITE_BUILD_NUMBER}"
 
 c=$(buildah from docker.io/caddy:latest)
 buildah add $c raku-doc-website.tar.gz /usr/share/caddy
 buildah add $c Caddyfile /etc/caddy/Caddyfile
-buildah commit --rm $c quay.io/colemanx/raku-doc-website:$image_tag
-buildah push quay.io/colemanx/raku-doc-website:$image_tag
+
+# Commit with latest tag
+buildah commit --rm $c quay.io/colemanx/raku-doc-website:latest
+
+# Create a new container for the versioned tag with expiration label
+c2=$(buildah from quay.io/colemanx/raku-doc-website:latest)
+buildah config --label quay_expiration=8w $c2
+buildah commit --rm $c2 quay.io/colemanx/raku-doc-website:$version_tag
+
+# Push both tags
+buildah push quay.io/colemanx/raku-doc-website:latest
+buildah push quay.io/colemanx/raku-doc-website:$version_tag
 
