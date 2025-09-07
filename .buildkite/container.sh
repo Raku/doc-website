4d48fa8ee885cbf4d44f0c92141a4af6d83ae4fe
Coleman McFarland <coleman@de.limited>
2025-09-07 19:09:44 -0400

    Duplicate build to add expiry label

--- .buildkite/container.sh
+++ .buildkite/container.sh
@@ -8,13 +8,13 @@ version_tag="v1-$(date +%Y%m%d)-${BUILDKITE_BUILD_NUMBER}"
 c=$(buildah from docker.io/caddy:latest)
 buildah add $c raku-doc-website.tar.gz /usr/share/caddy
 buildah add $c Caddyfile /etc/caddy/Caddyfile
-
-# Commit with latest tag
 buildah commit --rm $c quay.io/colemanx/raku-doc-website:latest
 
 # Create a new container for the versioned tag with expiration label
-c2=$(buildah from quay.io/colemanx/raku-doc-website:latest)
-buildah config --label quay_expiration=8w $c2
+c2=$(buildah from docker.io/caddy:latest)
+buildah add $c2 raku-doc-website.tar.gz /usr/share/caddy
+buildah add $c2 Caddyfile /etc/caddy/Caddyfile
+buildah config --label "quay_expiration=8w" $c2
 buildah commit --rm $c2 quay.io/colemanx/raku-doc-website:$version_tag
 
 # Push both tags
